90915771d8a6c1cd87454bb0d96beee6bc0aefeb
msigala <m96sigala@gmail.com>
2023-03-06 01:01:01 -0600

    Intial-start

---
+++ Assets/script.js
@@ -0,0 +1,36 @@
+//Define the questions and answers properties with an Array
+var quiz = [ 
+     {questionText: "What is JavaScript used for?",    
+     answerOptions: ["A. It adds style to our page", "B. Its adds interactivity to the page. ", "C. It adds structure to the page.", "D. IDK..."],
+    correctAnswer: "The Correct Answer is B! "
+  },
+  {
+    questionText: "What is the best practice when placing a 'script'?",
+    answerOptions: ["A. It doesnt matter where you place it, throw it down somewhere and it'll work just fine!", "B. Placing the script tag at the bottom of the HTML page, just before the closing body tag, is a recommended best practice"],
+    correctAnswer: "The Correct Answer is B! Placing the tag at the bottom of the body element can improve the loading speed and performance of the web page."
+  },
+  {
+    questionText: "A Web API is a set of rules and protocols that allow different software applications to communicate and exchange data with each other over the internet.",
+    answerOptions: ["True", "False"],
+    correctAnswer: "The Correct Answer is True!"
+  },
+  {
+    questionText: "A .... is triggered when a user interacts with the keyboard, such as typing a key or pressing a button.",
+    answerOptions: ["A. eventListener", "B. Event Bubble", "C. Keyboard Event", "D. Data Event"],
+    correctAnswer: "The Correct Answer is C, Keyboard Event!"
+  },
+  {
+    questionText: "Which of these selections is NOT used for Iteration?",
+    answerOptions: ["A. 'if'", "B. 'for'", "C. 'do-while'", "D. 'while'"],
+    correctAnswer: "The Correct Answer is A, 'if' is used in conditional statements!"
+  }
+];
+
+
+// Start the quiz
+
+  // Hide the start button
+ 
+// Check if answer is correct and display feedback
+
+// End the quiz and display the score
