4a1f8ba072074259beebf654aa3711309eba9e43
msigala <m96sigala@gmail.com>
2023-03-07 14:35:13 -0600

    working java

--- Assets/script.js
+++ Assets/script.js
@@ -1,36 +1,78 @@
 //Define the questions and answers properties with an Array
 var quiz = [ 
-     {questionText: "What is JavaScript used for?",    
-     answerOptions: ["A. It adds style to our page", "B. Its adds interactivity to the page. ", "C. It adds structure to the page.", "D. IDK..."],
-    correctAnswer: "The Correct Answer is B! "
+     {
+      questionText: "What is JavaScript used for?",    
+     answerOptions: [
+      {text: "A. It adds style to our page", correct:false},
+       {text: "B. Its adds interactivity to the page.", correct:true},
+       {text: "C. It adds structure to the page.",correct: false},
+       {text: "D. IDK...", correct: false}
+      ]
   },
   {
     questionText: "What is the best practice when placing a 'script'?",
-    answerOptions: ["A. It doesnt matter where you place it, throw it down somewhere and it'll work just fine!", "B. Placing the script tag at the bottom of the HTML page, just before the closing body tag, is a recommended best practice"],
-    correctAnswer: "The Correct Answer is B! Placing the tag at the bottom of the body element can improve the loading speed and performance of the web page."
+    answerOptions: [
+      {text: "A. It doesnt matter where you place it, throw it down somewhere and it'll work just fine!", correct:false},
+       {text: "B. Placing the script tag at the bottom of the HTML page, just before the closing body tag, is a recommended best practice", correct:true}
+      ]
   },
   {
     questionText: "A Web API is a set of rules and protocols that allow different software applications to communicate and exchange data with each other over the internet.",
-    answerOptions: ["True", "False"],
-    correctAnswer: "The Correct Answer is True!"
+    answerOptions: [
+      {text: "True", correct:true}, 
+      {text: "False", correct:false}
+    ]
   },
   {
     questionText: "A .... is triggered when a user interacts with the keyboard, such as typing a key or pressing a button.",
-    answerOptions: ["A. eventListener", "B. Event Bubble", "C. Keyboard Event", "D. Data Event"],
-    correctAnswer: "The Correct Answer is C, Keyboard Event!"
+    answerOptions: [
+      {text: "A. eventListener", correct:false}, 
+      {text: "B. Event Bubble", correct:false}, 
+      {text: "C. Keyboard Event", correct:true}, 
+      {text: "D. Data Event", correct:false}
+    ]
   },
   {
     questionText: "Which of these selections is NOT used for Iteration?",
-    answerOptions: ["A. 'if'", "B. 'for'", "C. 'do-while'", "D. 'while'"],
-    correctAnswer: "The Correct Answer is A, 'if' is used in conditional statements!"
+    answerOptions: [
+      {text: "A. 'if'", correct:true}, 
+      {text: "B. 'for'", correct:false}, 
+      {text: "C. 'do-while'", correct:false}, 
+      {text: "D. 'while'", correct:false}],
   }
 ];
 
+let timeInterval;
+let seconds = 75;
+let minutes = Math.floor(0); 
+let hours = 0;
+const questionContainer = document.getElementById('question-screen')
 
-// Start the quiz
+// Starts the quiz and hides the header ID
+const startTimer = () => {
+  document.getElementById('quiz-intro').style.display = 'none'; // hide start button when pressed
+  timerInterval = setInterval(() => {
+    seconds--;
+    if (seconds < 0) {
+      clearInterval(timerInterval);
+      document.getElementById('timer').innerText = 'Time\'s up!';
+      return;
+    }
+    const formattedTime = formatTime(hours, minutes, seconds);
+    document.getElementById('timer').innerText = formattedTime;
+  }, 1000);
+};
 
-  // Hide the start button
+const formatTime = (hours, minutes, seconds) => {
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+  return `${formattedMinutes}:${formattedSeconds}`;
+};
+
+document.getElementById('startButton').addEventListener('click', startTimer);
  
-// Check if answer is correct and display feedback
-
-// End the quiz and display the score
+// Display the Quiz content
+const startGame = () => {
+  questionContainer.classList.remove('hide')
+}
+//Ends the quiz, gives the leaderboard stats, and gives an option to start over.
